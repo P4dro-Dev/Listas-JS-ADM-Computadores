518243957e2d8158e67b0aa98643f2ac59ed0611
Pedro Henrique <Ph9084533@gmail.com>
2025-05-22 17:00:30 +0000

    Adiconando comentários ao script, para melhor compresão do visitante ao repositório

--- Lista-de-Exercícios-JS/Q-I.js
+++ Lista-de-Exercícios-JS/Q-I.js
@@ -1,4 +1,13 @@
 // Primeira questão: 
+// Crie um script JavaScript que:
+// a. Declare uma constante IDADE_MINIMA_DESCONTO com o valor 18.
+// b. Declare uma variável idadeCliente (usando let) e atribua a ela um valor numérico (ex: 25).
+// c. Declare uma variável tipoAssinaturaCliente (usando let) e atribua a ela uma string (ex: "Premium" ou "Basico").
+// d. Declare uma constante ASSINATURA_NECESSARIA com o valor "Premium".
+// e. Escreva uma estrutura condicional (if/else) que verifique se idadeCliente é maior ou igual a IDADE_MINIMA_DESCONTO se tipoAssinaturaCliente é estritamente(===) igual a ASSINATURA_NECESSARIA.
+// f. Se ambas as condições forem verdadeiras, exiba no console: "Parabéns! Você é elegível para o desconto especial."
+// g. Caso contrário, exiba: "Desculpe, você não atende aos critérios para o desconto especial."
+// h. Teste seu código com diferentes valores para idadeCliente e tipoAssinaturaCliente para ver os diferentes resultados.
 
 let pontuacao = 1001
 if ( pontuacao > 1000 ) {
